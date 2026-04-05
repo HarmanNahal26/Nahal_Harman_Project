304380a645cfe58c2c2db75f408047f3cecbe4fa
HarmanNahal26 <hnahal@academic.rrc.ca>
2026-04-05 14:39:59 -0500

    feat: categories closes #3

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,17 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import expenseRoutes from "./routes/expenseRoutes";
+import categoryRoutes from "./routes/categoryRoutes";
+
+
 
 const app = express();
-
-app.use("/expenses", expenseRoutes);
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use("/expenses", expenseRoutes);
+app.use("/categories", categoryRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("API Running");
